62340ff5dc87f3fc7a40afaf0a067a93d6fc1005
Mampuru <mampuruntwampe@gmail.com>
2024-04-19 20:13:55 +0200

    Addition of user and feedback models

---
+++ feedback-api/feedback-api/Models/Feedback.cs
@@ -0,0 +1,17 @@
+﻿namespace feedback_api.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public FeedbackIndicator Indicator { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+
+    public enum FeedbackIndicator
+    {
+        Success,
+        Failure
+    }
+}
---
+++ feedback-api/feedback-api/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace feedback_api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public UserRole Role { get; set; }
+    }
+
+    public enum UserRole
+    {
+        Admin,
+        Customer,
+        SuperUser
+    }
+}

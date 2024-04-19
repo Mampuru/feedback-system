4d98a31c9abf6e5017109f7bdfc124d644620891
Mampuru <mampuruntwampe@gmail.com>
2024-04-19 20:55:02 +0200

    Addition of the feedback controller

---
+++ feedback-api/feedback-api/Controllers/FeedbackController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using feedback_api.Data;
+using feedback_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace feedback_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FeedbackController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Policy = "Customer")]
+        [HttpPost]
+        public async Task<IActionResult> SubmitFeedback(Feedback feedback)
+        {
+            _context.Feedbacks.Add(feedback);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [Authorize(Policy = "Admin")]
+        [HttpGet]
+        public IActionResult GetFeedbacks()
+        {
+            var feedbacks = _context.Feedbacks.Include(f => f.User).ToList();
+            return Ok(feedbacks);
+        }
+    }
+}

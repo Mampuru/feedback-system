3e733055ff7a3138499b372b7ae85d5d7616a53f
Mampuru <mampuruntwampe@gmail.com>
2024-04-19 21:09:31 +0200

    Updated the db context added superuser model

---
+++ feedback-api/feedback-api/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using feedback_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace feedback_api.Data
+{
+    
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<SuperUser> SuperUsers { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Your_Connection_String_Here");
+        }
+    }
+}

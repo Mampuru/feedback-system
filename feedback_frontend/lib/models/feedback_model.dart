573224740574ed662e2b4baa2cfd5de9f1621b71
Mampuru <mampuruntwampe@gmail.com>
2024-04-21 23:48:52 +0200

    Addition of the feedback model

---
+++ feedback_frontend/lib/models/feedback_model.dart
@@ -0,0 +1,7 @@
+class FeedbackModel {
+  final String id;
+  final String indicator;
+  final String description;
+
+  FeedbackModel({required this.id, required this.indicator, required this.description});
+}

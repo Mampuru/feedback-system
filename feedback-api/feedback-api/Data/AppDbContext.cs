976bd54c9956b3dfe783c36dacae0523ffe82d40
Mampuru <mampuruntwampe@gmail.com>
2024-04-22 06:37:51 +0200

    Updated the db context added the class constructor

--- feedback-api/feedback-api/Data/AppDbContext.cs
+++ feedback-api/feedback-api/Data/AppDbContext.cs
@@ -7,14 +7,10 @@ namespace feedback_api.Data
 
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Feedback> Feedbacks { get; set; }
-        public DbSet<SuperUser> SuperUsers { get; set; }
 
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Your_Connection_String_Here");
-        }
     }
 }

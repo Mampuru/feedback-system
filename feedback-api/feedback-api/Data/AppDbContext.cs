be837b120e32ee9544780776673a27e0e0b57509
Mampuru <mampuruntwampe@gmail.com>
2024-04-23 18:02:42 +0200

    Code clean up

--- feedback-api/feedback-api/Data/AppDbContext.cs
+++ feedback-api/feedback-api/Data/AppDbContext.cs
@@ -3,8 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace feedback_api.Data
 {
-    
-
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }

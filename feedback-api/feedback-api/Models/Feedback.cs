ca1e3eb43c3ab019058165249c7b67274b36369a
Mampuru <mampuruntwampe@gmail.com>
2024-04-23 18:04:43 +0200

    Change th nullable status of the user model fields

--- feedback-api/feedback-api/Models/Feedback.cs
+++ feedback-api/feedback-api/Models/Feedback.cs
@@ -3,10 +3,10 @@
     public class Feedback
     {
         public int Id { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public FeedbackIndicator Indicator { get; set; }
         public int UserId { get; set; }
-        public User User { get; set; }
+        public User? User { get; set; }
     }
 
     public enum FeedbackIndicator

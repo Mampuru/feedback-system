356055399367f3d69caee02245bb64629831b235
Mampuru <mampuruntwampe@gmail.com>
2024-04-23 18:26:09 +0200

    Changed Username to a required field

--- feedback-api/feedback-api/Models/User.cs
+++ feedback-api/feedback-api/Models/User.cs
@@ -3,7 +3,7 @@
     public class User
     {
         public int Id { get; set; }
-        public string? Username { get; set; }
+        public required string Username { get; set; }
         public required string PasswordHash { get; set; }
         public required string Email { get; set; }
         public UserRole Role { get; set; }

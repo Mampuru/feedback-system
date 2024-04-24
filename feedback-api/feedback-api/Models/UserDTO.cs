aa101e08e0641920bb1519ae6dd5f014373a763b
Mampuru <mampuruntwampe@gmail.com>
2024-04-24 02:50:19 +0200

    Addition of a data transfer object for user model

---
+++ feedback-api/feedback-api/Models/UserDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace feedback_api.Models
+{
+    public class UserDTO
+    {
+        public string? Username { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+        public UserRole Role { get; set; }
+
+        public static User convertToUser(UserDTO user)
+        {
+            User x = new()
+            {
+                Username = user.Username,
+                PasswordHash = user.Password,
+                Email = user.Email,
+                Role = user.Role
+            };
+
+            return x;
+        }
+    }
+
+}
+

ae3a913ab68a6b4973e056fb535d142eaf4a2502
Mampuru <mampuruntwampe@gmail.com>
2024-04-24 05:25:03 +0200

    Change service to config file

---
+++ feedback_frontend/lib/api/api_config.dart
@@ -0,0 +1,12 @@
+
+String baseURL = "https://localhost:7153";
+
+String loginURL = "$baseURL/api/User/login";
+String signupURL = "$baseURL/api/User/signup";
+
+String feedbackURL = "$baseURL/api/Feedback";
+
+String addAdminURL = "$baseURL/api/User/addAdmin";
+String delAdminURL = "$baseURL/api/User/deleteAdmin";
+String updateAdminURL = "$baseURL/api/User/updateAdmin";
+String getAdminURL = "$baseURL/api/User/viewAdmins";
